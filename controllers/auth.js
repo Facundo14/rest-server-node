0651d1423bc7a7c6d19af548987180038ef46c9a
Facundo14 <facundotanovich@gmail.com>
2022-02-17 15:52:43 -0300

    Final Seccion 11 - Google SignIn agregado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const { response } = require('express');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const Usuario = require('../models/usuario');
 
@@ -57,8 +58,60 @@ const login = async(req, res = response) => {
     }
 }
 
+const googleSignIn = async( req, res = response) => {
+    const {id_token} = req.body;
+
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if(!usuario){
+            //Tengo que crear el usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':D',
+                img,
+                google: true
+            };  
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en DB
+        if(!usuario.estado){
+            return res.status(401).json({
+                ok: false,
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        //Generar JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            usuario,
+            token
+        });
+
+    } catch (error) {
+
+        console.log(error)
+        return res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        }); 
+        
+    }
+
+}
+
 
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
